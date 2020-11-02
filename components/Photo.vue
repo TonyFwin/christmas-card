aaf1482d986f6e6a35b846e93950fe3a706ab06f
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2020-11-01 20:59:28 -0600

    Create photo component

---
+++ components/Photo.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="my-8">
+    <div class="container mx-auto">
+      <img class="rounded-sm shadow-lg mx-auto max-w-xl" src="~/assets/images/paddys.jpg" alt="">
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
