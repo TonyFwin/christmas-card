1d7c3240011b7b6bf33471cb291ca0399e69d4ce
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2020-11-01 20:59:05 -0600

    Create and style greeting component

---
+++ components/Greeting.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="my-8">
+    <div class="container m-auto flex flex-col text-center">
+      <h1 class="text-4xl md:text-5xl lg:text-6xl font-medium">
+        Seasons Greetings from the Nguyen Family!
+      </h1>
+      <p class="mt-2 leading-loose text-lg md:text-2xl">
+        Wish you all the best and most wonderful things this holiday season!
+      </p>
+    </div>
+    <hr class="mt-8 w-1/6 mx-auto border-2 border-red-500">
+  </section>
+</template>
+<style>
+</style>
