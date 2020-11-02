ddbadca7ffe7140337ea81fd101bbeca14a49999
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2020-11-01 22:03:15 -0600

    Update background color and remove nuxt classes

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mx-auto">
-    <div>
+    <div class="bg-gray-300">
       <Photo />
       <Greeting />
       <Moving />
@@ -12,30 +12,4 @@
 export default {}
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-
-.title {
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
