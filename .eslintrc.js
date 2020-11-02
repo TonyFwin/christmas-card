6e0e7b698ac50bfb1b721a5799f1d6fec2aed3e8
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2020-11-01 21:01:43 -0600

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,4 +17,19 @@ module.exports = {
   plugins: ['prettier'],
   // add your custom rules here
   rules: {},
+  build: {
+    extend(config, ctx) {
+        if (ctx.isDev && ctx.isClient) {
+            config.module.rules.push({
+                enforce: 'pre',
+                test: /\.(js|vue)$/,
+                loader: 'esline-loader',
+                exclude: /(node_modules)/,
+                options: {
+                    fix: true
+                }
+            })
+        }
+    }
+  }
 }
