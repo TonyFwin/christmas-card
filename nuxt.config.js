49d0b01c925832a47496304097337a7892006b2d
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2020-11-01 21:01:05 -0600

    Add google fonts to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,11 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans&display=swap'},
+         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Mountains+of+Christmas:wght@400;700&display=swap'}
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
