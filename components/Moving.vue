9ca395e85c7e5c3156ada8f47f5d553196abeb77
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2020-11-01 21:00:44 -0600

    Cretae moving component

---
+++ components/Moving.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="my-8">
+    <div class="container m-auto flex flex-col text-center">
+    </div>
+    <hr class="mt-8 w-1/6 mx-auto border-2 border-green-500">
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
