8d8c64c7812912b10c64310c229ebdf18c0b6cfc
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2020-11-02 21:58:42 -0600

    Add copy and placeholder image to moving component

--- components/Moving.vue
+++ components/Moving.vue
@@ -1,7 +1,50 @@
 <template>
-  <section class="py-8 md:py-16">
-    <div class="container m-auto flex flex-col text-center"></div>
-    <hr class="mt-8 w-1/6 mx-auto border-2 border-green-500" />
+  <section class="py-8 md:py-12">
+    <div class="container m-auto flex flex-wrap text-center">
+      <div class="w-full md:w-5/12 mr-auto">
+        <img
+          class="max-w-full rounded-lg shadow-lg"
+          src="~/assets/images/paddys.jpg"
+          alt=""
+        />
+      </div>
+      <div class="w-full md:w-6/12 mx-auto">
+        <h3
+          class="text-4xl font-semibold px-4 flex items-center justify-center"
+        >
+          We Moved to Minneapolis
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="ml-3"
+            width="44"
+            height="44"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#4CAF50"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <circle cx="7" cy="17" r="2" />
+            <circle cx="17" cy="17" r="2" />
+            <path
+              d="M5 17h-2v-11a1 1 0 0 1 1 -1h9v12m-4 0h6m4 0h2v-6h-8m0 -5h5l3 5"
+            />
+          </svg>
+        </h3>
+        <p class="mt-4 text-gray-800">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+    <hr class="mt-8 md:mt-12 w-1/6 mx-auto border-2 border-green-500" />
   </section>
 </template>
 
