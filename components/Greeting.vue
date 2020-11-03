d307a37c03b78a4fc4d1dcb2076ddf9758e8664e
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2020-11-02 21:55:36 -0600

    Update the spacing and text color of the greeting block

--- components/Greeting.vue
+++ components/Greeting.vue
@@ -1,14 +1,14 @@
 <template>
-  <section class="py-8 md:py-16">
+  <section class="py-8 md:py-12">
     <div class="container m-auto flex flex-col text-center">
       <h1 class="text-4xl md:text-5xl lg:text-6xl font-medium">
         Seasons Greetings from the Nguyen Family!
       </h1>
-      <p class="mt-2 leading-loose text-lg md:text-2xl">
+      <p class="mt-2 leading-loose text-lg md:text-2xl text-gray-800">
         Wish you all the best and most wonderful things this holiday season!
       </p>
     </div>
-    <hr class="mt-8 w-1/6 mx-auto border-2 border-red-500" />
+    <hr class="mt-8 md:mt-12 w-1/6 mx-auto border-2 border-red-500" />
   </section>
 </template>
 <style></style>
