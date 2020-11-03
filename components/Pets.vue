e461013fda90428d0566333165fa52a231dd2b4b
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2020-11-02 21:58:22 -0600

    Update copy, and layout of pets component

--- components/Pets.vue
+++ components/Pets.vue
@@ -1,13 +1,45 @@
 <template>
-
+  <section class="py-8 md:py-12">
+    <div class="container mx-auto text-center mb-8">
+      <h2 class="text-4xl font-semibold">The "Kids"</h2>
+    </div>
+    <div class="container m-auto grid grid-cols-2 gap-8 md:gap-12 text-center">
+      <div class="flex flex-col">
+        <img
+          class="max-w-full rounded-lg shadow-lg"
+          src="~/assets/images/paddys.jpg"
+          alt=""
+        />
+        <h3 class="mt-4 font-semibold text-2xl">Marley</h3>
+        <ul class="mt-4">
+          <li>Marley Turned 6 this year</li>
+          <li>She also lost a tooth</li>
+          <li>She's been loving patio's in the Twin Cities</li>
+        </ul>
+      </div>
+      <div class="flex flex-col">
+        <img
+          class="max-w-full rounded-lg shadow-lg"
+          src="~/assets/images/paddys.jpg"
+          alt=""
+        />
+        <h3 class="mt-4 font-semibold text-2xl">Diana</h3>
+        <ul class="mt-4">
+          <li>Marley Turned 3 this year</li>
+          <li>The vet said she needs to lose a pound or two</li>
+          <li>
+            Her favorite pasttime at the new place is threatening the alleyway
+            squirrels
+          </li>
+        </ul>
+      </div>
+    </div>
+    <hr class="mt-8 md:mt-12 w-1/6 mx-auto border-2 border-green-500" />
+  </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
