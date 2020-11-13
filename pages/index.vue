2be7b21a817bbc2ee4b55379e9386a7c134167db
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2020-11-12 21:53:23 -0600

    Add lights and gallary to page, and add horizonal padding to entire page

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="mx-auto">
-    <div class="bg-gray-300">
+    <div class="bg-gray-300 px-4">
+      <Lights />
       <Photo />
       <Greeting />
       <Moving />
       <Anniversary />
       <Pets />
+      <Gallery />
     </div>
   </div>
 </template>
-
-<script>
-export default {}
-</script>
-<style></style>
