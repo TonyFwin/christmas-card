2b234953173ed311c500a0eceec4983af20d75b7
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2020-11-12 21:51:46 -0600

    Crete gallary section and fill with placeholder images

---
+++ components/Gallery.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="py-8 md:py-12">
+    <div class="container mx-auto text-center mb-8">
+      <h2 class="text-4xl font-semibold">
+        Merry Christmas and Happy New Year!
+      </h2>
+      <p class="mt-4 text-gray-800">
+        We hope you enjoyed our little Christmas 'card'. Stay safe, stay warm
+        and have a Happy Holidays from the Nguyen family!
+      </p>
+    </div>
+    <div
+      class="container m-auto grid grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 text-center"
+    >
+      <img
+        class="max-w-full rounded-lg shadow-lg"
+        src="~/assets/images/paddys.jpg"
+        alt=""
+      /><img
+        class="max-w-full rounded-lg shadow-lg"
+        src="~/assets/images/paddys.jpg"
+        alt=""
+      /><img
+        class="max-w-full rounded-lg shadow-lg"
+        src="~/assets/images/paddys.jpg"
+        alt=""
+      /><img
+        class="max-w-full rounded-lg shadow-lg"
+        src="~/assets/images/paddys.jpg"
+        alt=""
+      /><img
+        class="max-w-full rounded-lg shadow-lg"
+        src="~/assets/images/paddys.jpg"
+        alt=""
+      /><img
+        class="max-w-full rounded-lg shadow-lg"
+        src="~/assets/images/paddys.jpg"
+        alt=""
+      />
+    </div>
+  </section>
+</template>
+<script>
+export default {}
+</script>
+
+<style></style>
