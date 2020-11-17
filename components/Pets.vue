4fb87d162cdcf68af85f501027a52157c35521c3
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2020-11-16 21:56:06 -0600

    Rework pets section entirely

--- components/Pets.vue
+++ components/Pets.vue
@@ -1,81 +1,73 @@
 <template>
-  <section class="py-8 md:py-12">
+  <section class="py-4 md:py-8">
     <div class="container mx-auto text-center mb-8">
-      <h2 class="text-4xl font-semibold">The "Kids"</h2>
+      <h2 class="text-4xl font-semibold flex justify-center items-center">
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="cat"
+          class="svg-inline--fa fa-cat w-8 mr-4 text-gray-600"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path
+            fill="currentColor"
+            d="M290.59 192c-20.18 0-106.82 1.98-162.59 85.95V192c0-52.94-43.06-96-96-96-17.67 0-32 14.33-32 32s14.33 32 32 32c17.64 0 32 14.36 32 32v256c0 35.3 28.7 64 64 64h176c8.84 0 16-7.16 16-16v-16c0-17.67-14.33-32-32-32h-32l128-96v144c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V289.86c-10.29 2.67-20.89 4.54-32 4.54-61.81 0-113.52-44.05-125.41-102.4zM448 96h-64l-64-64v134.4c0 53.02 42.98 96 96 96s96-42.98 96-96V32l-64 64zm-72 80c-8.84 0-16-7.16-16-16s7.16-16 16-16 16 7.16 16 16-7.16 16-16 16zm80 0c-8.84 0-16-7.16-16-16s7.16-16 16-16 16 7.16 16 16-7.16 16-16 16z"
+          ></path>
+        </svg>
+        <span>The "Kids"</span>
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="dog"
+          class="svg-inline--fa fa-dog w-8 ml-4"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 576 512"
+        >
+          <path
+            fill="currentColor"
+            d="M298.06,224,448,277.55V496a16,16,0,0,1-16,16H368a16,16,0,0,1-16-16V384H192V496a16,16,0,0,1-16,16H112a16,16,0,0,1-16-16V282.09C58.84,268.84,32,233.66,32,192a32,32,0,0,1,64,0,32.06,32.06,0,0,0,32,32ZM544,112v32a64,64,0,0,1-64,64H448v35.58L320,197.87V48c0-14.25,17.22-21.39,27.31-11.31L374.59,64h53.63c10.91,0,23.75,7.92,28.62,17.69L464,96h64A16,16,0,0,1,544,112Zm-112,0a16,16,0,1,0-16,16A16,16,0,0,0,432,112Z"
+          ></path>
+        </svg>
+      </h2>
     </div>
-    <div class="container m-auto grid grid-cols-2 gap-8 md:gap-12 text-center">
-      <div class="flex flex-col">
-        <img
-          class="max-w-full rounded-lg shadow-lg"
-          src="~/assets/images/merj.jpeg"
-          alt=""
-        />
-        <h3
-          class="mt-4 font-semibold text-2xl flex items-center justify-center"
-        >
-          <span>Marley</span>
-          <svg
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fas"
-            data-icon="dog"
-            class="svg-inline--fa fa-dog w-8 ml-2"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 576 512"
-          >
-            <path
-              fill="currentColor"
-              d="M298.06,224,448,277.55V496a16,16,0,0,1-16,16H368a16,16,0,0,1-16-16V384H192V496a16,16,0,0,1-16,16H112a16,16,0,0,1-16-16V282.09C58.84,268.84,32,233.66,32,192a32,32,0,0,1,64,0,32.06,32.06,0,0,0,32,32ZM544,112v32a64,64,0,0,1-64,64H448v35.58L320,197.87V48c0-14.25,17.22-21.39,27.31-11.31L374.59,64h53.63c10.91,0,23.75,7.92,28.62,17.69L464,96h64A16,16,0,0,1,544,112Zm-112,0a16,16,0,1,0-16,16A16,16,0,0,0,432,112Z"
-            ></path>
-          </svg>
-        </h3>
-        <ul class="mt-4">
-          <li>Marley Turned 6 this year</li>
-          <li>She also lost a tooth</li>
-          <li>
-            She's been loving patio life in the Twin Cities as well as visiting
-            her new favorite spot pictured above.
-          </li>
-        </ul>
+
+    <div class="container bg-gray-100 m-auto flex flex-wrap shadow-sm">
+      <div class="w-full md:w-6/12 mr-auto">
+        <img class="max-w-full" src="~/assets/images/merj.jpeg" alt="" />
       </div>
-      <div class="flex flex-col">
-        <img
-          class="max-w-full rounded-lg shadow-lg"
-          src="~/assets/images/snugdiana.jpeg"
-          alt=""
-        />
-        <h3
-          class="mt-4 font-semibold text-2xl flex items-center justify-center"
-        >
-          <span>Diana</span>
-          <svg
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fas"
-            data-icon="cat"
-            class="svg-inline--fa fa-cat w-8 ml-2"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-          >
-            <path
-              fill="currentColor"
-              d="M290.59 192c-20.18 0-106.82 1.98-162.59 85.95V192c0-52.94-43.06-96-96-96-17.67 0-32 14.33-32 32s14.33 32 32 32c17.64 0 32 14.36 32 32v256c0 35.3 28.7 64 64 64h176c8.84 0 16-7.16 16-16v-16c0-17.67-14.33-32-32-32h-32l128-96v144c0 8.84 7.16 16 16 16h32c8.84 0 16-7.16 16-16V289.86c-10.29 2.67-20.89 4.54-32 4.54-61.81 0-113.52-44.05-125.41-102.4zM448 96h-64l-64-64v134.4c0 53.02 42.98 96 96 96s96-42.98 96-96V32l-64 64zm-72 80c-8.84 0-16-7.16-16-16s7.16-16 16-16 16 7.16 16 16-7.16 16-16 16zm80 0c-8.84 0-16-7.16-16-16s7.16-16 16-16 16 7.16 16 16-7.16 16-16 16z"
-            ></path>
-          </svg>
-        </h3>
-        <ul class="mt-4">
-          <li>Diana Turned 3 this year!</li>
-          <li>The vet said she needs to lose a pound or two</li>
-          <li>
-            Her new favorite pasttime is threatening the alleyway birds and
-            squirrels from our many window sills.
-          </li>
-        </ul>
+      <div class="w-full md:w-6/12 mx-auto flex flex-col justify-center px-8">
+        <h3 class="font-semibold text-4xl flex items-center">Marley</h3>
+        <p class="mt-8">
+          Marley has had an eventful 2020. She celebrated her 6th birthday in
+          January. She also went to the vet for ouchies in both her front ankles
+          and had to have a molar removed. She's been enjoying Minneapolis
+          patios and being very close the river. Marley is loving the work from
+          home life and has been taking full advantage of Tony's new job.
+        </p>
       </div>
     </div>
-    <hr class="mt-8 md:mt-12 w-1/6 mx-auto border-2 border-green-600" />
+
+    <div class="container bg-gray-100 m-auto flex flex-wrap shadow-sm mt-8">
+      <div class="w-full md:w-6/12 mx-auto flex flex-col justify-center px-8">
+        <h3 class="font-semibold text-4xl flex items-center">Diana</h3>
+        <p class="mt-8">
+          Diana has also had an eventful year! She celebrated her 3rd birthday
+          in October. The vet told her she needed to lose a pound or two. She
+          has a new favorite pastime which includes sitting in the window and
+          chirping at the birds and squirrels in the alleyway as well as
+          screaming for food before sunrise.
+        </p>
+      </div>
+      <div class="w-full md:w-6/12 mr-auto">
+        <img class="max-w-full" src="~/assets/images/snugdiana.jpeg" alt="" />
+      </div>
+    </div>
+    <hr class="mt-8 md:mt-16 w-1/6 mx-auto border-2 border-green-600" />
   </section>
 </template>
 
