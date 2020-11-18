6f4dc8900dd0fd79f2abd29513c031bb28fa8f16
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2020-11-17 21:43:37 -0600

    Remove one svg, update spacing, alignment, and width for mobile

--- components/Anniversary.vue
+++ components/Anniversary.vue
@@ -1,37 +1,17 @@
 <template>
   <section class="py-4 md:py-8">
     <div class="container m-auto flex flex-wrap">
-      <div class="w-full md:w-5/12 ml-auto order-1">
+      <div class="w-full md:w-5/12 ml-auto order-0 lg:order-1 my-8 md:my-auto">
         <img
-          class="max-w-full rounded-lg shadow-lg"
+          class="w-full md:max-w-full rounded-lg shadow-lg"
           src="~/assets/images/sunflowers.jpg"
           alt=""
         />
       </div>
       <div class="w-full md:w-6/12 mx-auto flex flex-col justify-center">
-        <h3 class="text-4xl font-semibold px-4 flex items-center mb-8">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="40"
-            height="40"
-            fill="#38a169"
-            viewBox="0 0 256 256"
-            class="mr-3"
-          >
-            <rect width="256" height="256" fill="none"></rect>
-            <path
-              d="M128.0293,216s-100-56-100-124A52.00881,52.00881,0,0,1,128.02938,71.965l-.0002.00008A52.00882,52.00882,0,0,1,228.0293,92C228.0293,160,128.0293,216,128.0293,216Z"
-              opacity="0.2"
-            ></path>
-            <path
-              d="M128.0293,216s-100-56-100-124A52.00881,52.00881,0,0,1,128.02938,71.965l-.0002.00008A52.00882,52.00882,0,0,1,228.0293,92C228.0293,160,128.0293,216,128.0293,216Z"
-              fill="none"
-              stroke="#38a169"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="16"
-            ></path>
-          </svg>
+        <h3
+          class="text-3xl lg:text-4xl font-semibold px-4 flex items-center my-8 md:mb-8 justify-center lg:justify-start"
+        >
           We celebrated our 4th anniversary
           <svg
             xmlns="http://www.w3.org/2000/svg"
--- components/Moving.vue
+++ components/Moving.vue
@@ -1,15 +1,17 @@
 <template>
   <section class="py-4 md:py-8">
     <div class="container m-auto flex flex-wrap">
-      <div class="w-full md:w-5/12 mr-auto">
+      <div class="w-full md:w-5/12 m-auto">
         <img
-          class="max-w-full rounded-lg shadow-lg"
+          class="w-full md:max-w-full rounded-lg shadow-lg"
           src="~/assets/images/nordeast.png"
           alt=""
         />
       </div>
       <div class="w-full md:w-6/12 mx-auto flex flex-col justify-center">
-        <h3 class="text-4xl font-semibold px-4 flex items-center mb-8">
+        <h3
+          class="text-3xl lg:text-4xl font-semibold px-4 flex items-center my-8 md:mb-8 justify-center md:justify-start"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="40"
@@ -96,90 +98,8 @@
             ></line>
           </svg>
           We Moved to Minneapolis
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="40"
-            height="40"
-            fill="#38a169"
-            viewBox="0 0 256 256"
-            class="ml-3"
-          >
-            <rect width="256" height="256" fill="none"></rect>
-            <path
-              d="M176,144H16v40a8,8,0,0,0,8,8H44a24,24,0,0,1,48,0h72a23.99048,23.99048,0,0,1,11.99813-20.78815Z"
-              opacity="0.2"
-            ></path>
-            <path
-              d="M212,192a24.00631,24.00631,0,0,0-36.00187-20.78815L176,120h64v64a8,8,0,0,1-8,8H212"
-              opacity="0.2"
-            ></path>
-            <path
-              d="M240,120H176V80h42.58374a8,8,0,0,1,7.42781,5.02887Z"
-              fill="none"
-              stroke="#e55e3e"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="16"
-            ></path>
-            <line
-              x1="16"
-              y1="144"
-              x2="176"
-              y2="144"
-              fill="none"
-              stroke="#e55e3e"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="16"
-            ></line>
-            <circle
-              cx="188"
-              cy="192"
-              r="24"
-              fill="none"
-              stroke="#e55e3e"
-              stroke-miterlimit="10"
-              stroke-width="16"
-            ></circle>
-            <circle
-              cx="68"
-              cy="192"
-              r="24"
-              fill="none"
-              stroke="#e55e3e"
-              stroke-miterlimit="10"
-              stroke-width="16"
-            ></circle>
-            <line
-              x1="164"
-              y1="192"
-              x2="92"
-              y2="192"
-              fill="none"
-              stroke="#e55e3e"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="16"
-            ></line>
-            <path
-              d="M44,192H24a8,8,0,0,1-8-8V72a8,8,0,0,1,8-8H176V171.21508"
-              fill="none"
-              stroke="#e55e3e"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="16"
-            ></path>
-            <path
-              d="M176,171.21508V120h64v64a8,8,0,0,1-8,8H212"
-              fill="none"
-              stroke="#e55e3e"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="16"
-            ></path>
-          </svg>
         </h3>
-        <p class="mt-4 text-gray-800">
+        <p class="md:mt-4 text-gray-800">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
